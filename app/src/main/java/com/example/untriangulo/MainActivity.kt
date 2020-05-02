f5aeda3dead4b18ffbd498a7f6833e8458df7c38
albertojrz <64441207+albertojrz@users.noreply.github.com>
2020-05-02 10:24:39 -0600

    Añadiendo el image view y la clase para dibujar fiuras geometricas

--- app/src/main/java/com/example/untriangulo/MainActivity.kt
+++ app/src/main/java/com/example/untriangulo/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.untriangulo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +12,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("quiobole prros")
+
+        val myDrawing = MyDrawable()
+        val image: ImageView = findViewById(R.id.imageView)
+        image.setImageDrawable(myDrawing)
+        image.contentDescription = resources.getString(R.string.app_name)
 
     }
 }
